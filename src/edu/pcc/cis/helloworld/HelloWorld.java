3b1f0ef3468f8972eb40e88a158b363ef9a5331e
ethompson <fake@faker.net>
2017-02-07 09:18:59 -0800

    No

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld
     public static void main( String[] args)
     {
         System.out.println("Hello World.");
-        System.out.println("Babbys first github push... -_-")
+        System.out.println("Babbys first github push... -_-");
+        System.out.println("Third commit just to make sure...")
     }
 }
