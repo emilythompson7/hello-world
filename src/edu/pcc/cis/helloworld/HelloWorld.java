a7de5057381e250fdf417062845a482326a95f6c
ethompson <fake@faker.net>
2017-02-07 09:10:21 -0800

    No

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -3,5 +3,10 @@ package edu.pcc.cis.helloworld;
 /**
  * Created by ethompson on 2/7/2017.
  */
-public class HelloWorld {
+public class HelloWorld
+{
+    public static void main( String[] args)
+    {
+        System.out.println("Hello World.");
+    }
 }
